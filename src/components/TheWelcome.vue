8f75b4587afc1d9cdd1da359d7c1b29c569e6e05
krisnapetot <krisnasagawa@gmail.com>
2023-07-05 07:53:19 +0700

    frist commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Projects</template>
+    You can see 
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">Projects</a>
+     at the company
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Service</template>
+    You can order
+    <a href="https://hpu-mining.com/services/" target="_blank" rel="noopener">Service</a> at the company
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Careers</template>
+    take a look at the
+    <a href="https://hpu-mining.com/careers/" target="_blank" rel="noopener">Careers</a> of the company
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Living at HPU</template>
+
+    You can see
+    <a href="https://hpu-mining.com/living-with-our-people/" target="_blank" rel="noopener">Living with our people</a> and
+    <a href="https://hpu-mining.com/living-with-our-community/" target="_blank" rel="noopener">Living with our community</a>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Contact us</template>
+
+    do you have any questions? You can
+    <a href="https://hpu-mining.com/contact-us/" target="_blank" rel="noopener">Contact us</a> to find the answer
+  </WelcomeItem>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about us">
+    <h2>Harmoni Panca Utama, PT. (HPU), is a private company established on the 25th of January 2011 which focuses only on the mining service.</h2>
+    <br />
+    <h2>The management is confident that it will grow and prosper alongside the development of mining industry in Indonesia with the strength of its experienced personnel with worldwide accepted quality standard competencies in the mining services industry</h2>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about us {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
